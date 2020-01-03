f8b3a196fc33c24bd38ac65215537d21ffc61b6e
Martin Riedel <web@riedel-privat.de>
2020-01-03 13:25:59 -0600

    Adapted case checker to own requirements.

--- README.md
+++ README.md
@@ -1,9 +1,26 @@
+# USCIS Case Checker
+
+Yet another USCIS Case Checker. My requirement was that i wanted to be able 
+to create one or more history files in csv (per case or one for multiple cases)
+and only be notified via telegram if the current status changes.
+
+Furthermore I wanted the individual script execution to be handled by `cron`.
+
 ### Usage:
 
 ```bash
-USCIS_CASE_ID='YOUR CASE ID' TELEGRAM_BOT_API='YOUR TELEGRAM BOT TOKEN' TELEGRAM_ID='YOUR CHAT_ID' python3 status_check.py
+usage: status_check.py [-h] -r RECEIPTS [RECEIPTS ...] [-f FILE]
+                       [-t BOT_TOKEN CHAT_ID]
+
+USCIS Case Checker and Telegram Notifier
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -r RECEIPTS [RECEIPTS ...], --receipts RECEIPTS [RECEIPTS ...]
+                        List of USCIS Receipt Numbers
+  -f FILE, --file FILE  Cache / History File
+  -t BOT_TOKEN CHAT_ID, --telegram BOT_TOKEN CHAT_ID
+                        Telegram Config
 ```
 
-It is defaulted to repeatedly check status every 12 hours, and send a message to specified telegram chat once a different status has been found.
-
-Docker version can be found: https://github.com/an0sunshy/dockerfiles/tree/master/uscis-status-checker
+Please do not spam USCIS by using that script and only query in moderate time frames.
--- status_check.py
+++ status_check.py
@@ -1,86 +1,144 @@
-import os
-import sys
-import time
-import requests
+#!/usr/bin/env python3
+
+import re
+import argparse
+import csv
+
+import telegram
+
 import logging
 from requests_html import HTMLSession
+from datetime import datetime, timezone
 
 logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    level=logging.WARNING,
+    format='%(levelname)s - %(message)s')
 URL = 'https://egov.uscis.gov/casestatus/mycasestatus.do'
 USER_AGENT = '"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36"'
 HEADER = {'user-agent': USER_AGENT}
 XPATH_STATUS = '/html/body/div[2]/form/div/div[1]/div/div/div[2]/div[3]/h1'
 XPATH_DESCRIPTION = '/html/body/div[2]/form/div/div[1]/div/div/div[2]/div[3]/p'
-TELEGRAM_API = 'https://api.telegram.org/bot{}/{}?chat_id={}&text={}'
+
+# 3 letters, 10 digits.
+USCIS_CASE_PATTERN = re.compile("^[A-Z]{3}\d{10}$")
 
 
-def check_status(id):
-    payload = {'initCaseSearch': 'CHECK STATUS', 'appReceiptNum': id}
+def check_status(number):
+    payload = {'initCaseSearch': 'CHECK STATUS', 'appReceiptNum': number}
     with HTMLSession() as s:
         r = s.post(URL, headers=HEADER, data=payload)
-        status = r.html.xpath(XPATH_STATUS, first=True).full_text
-        info = r.html.xpath(XPATH_DESCRIPTION, first=True).full_text
-    return status, info
+        status = r.html.xpath(XPATH_STATUS, first=True)
+        if status is None:
+            logging.info('Receipt number {} not found at USCIS or website down.'.format(number))
+            return 'Case not found', 'Seems like USCIS does not have a case for receipt number {}'.format(number)
+
+        info = r.html.xpath(XPATH_DESCRIPTION, first=True)
+        info_text = info.full_text if info is not None else 'No detailed description of status'
+
+    return status.full_text, info_text
 
 
-def send_notification(telegram_bot_api, telegram_id, content):
-    r = requests.get(
-        TELEGRAM_API.format(
-            telegram_bot_api,
-            'sendMessage',
-            telegram_id,
-            content), headers=HEADER)
-    if r.status_code != 200:
-        logging.error(
-            'Message failed to be sent to chat {}'.format(telegram_id))
+def send_notifications(telegram_bot_api_token, telegram_chat_id, contents):
+    bot = telegram.Bot(token=telegram_bot_api_token)
+
+    for c in contents:
+        number, status, info = c
+        message = \
+'''Immigration Update! USCIS case {} changed.
+        
+{}
+      
+{}'''.format(number, status, info)
+        logging.info(message)
+
+        max_length = telegram.constants.MAX_MESSAGE_LENGTH - 3
+        message = (message[:max_length] + '..') if len(message) > max_length else message
+
+        bot.send_message(telegram_chat_id, message)
 
 
-def check_periodically(id, telegram_bot_api, telegram_id):
-    records = []
-    with open('status_log', 'a+') as f:
-        for line in f:
-            status, info = line.split(': ')
-            records.append(status)
-        while True:
-            status, info = check_status(id)
-            if not records:
-                logging.info('First status check completed')
-                content = 'Current Status:\n{}\n{}'.format(status, info)
-                send_notification(telegram_bot_api, telegram_id, content)
-                records.append(status)
-                f.write("{}: {}".format(status, info))
-                f.flush()
-            elif status != records[-1]:
-                logging.info('New Status found: {}'.format(status))
-                content = 'New Status Found:\n{}\n{}'.format(status, info)
-                send_notification(telegram_bot_api, telegram_id, content)
-                records.append(status)
-                f.write("{}: {}".format(status, info))
-                f.flush()
-            else:
-                logging.info('Status has not been changed')
-            try:
-                time.sleep(12 * 60 * 60)
-            except KeyboardInterrupt:
-                logging.info('Program exits on C-c')
-                sys.exit(0)
+def check_receipt_number(number):
+    return USCIS_CASE_PATTERN.match(number)
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='USCIS Case Checker and Telegram Notifier')
+    parser.add_argument('-r', '--receipts',
+                        required=True, nargs='+', type=str, help='List of USCIS Receipt Numbers')
+    parser.add_argument('-f', '--file',
+                        required=False, type=str, help='Cache / History File')
+    parser.add_argument('-t', '--telegram',
+                        required=False, nargs=2, type=str, help='Telegram Config',
+                        metavar=('BOT_TOKEN', 'CHAT_ID'))
+    return parser.parse_args()
+
+
+def read_cases_cache_file(filename):
+    cases = {}
+    try:
+        with open(filename) as csv_file:
+            reader = csv.reader(csv_file)
+            # Read top down. Last status (bottom) represents current status value.
+            for row in reader:
+                # Valid List needs Receipt Number in row[1] and current status in row[2]
+                if len(row) < 2 or not check_receipt_number(row[1]):
+                    logging.warning('Invalid row in file: {}'.format(row))
+                    continue
+
+                cases[row[1]] = row[2]
+
+    except FileNotFoundError:
+        logging.warning('No file {} exits (yet). Assuming empty cache.'.format(filename))
+
+    return cases
+
+
+def write_cases_cache_file(rows, filename):
+    try:
+        with open(filename, mode='a') as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerows(rows)
+    except Exception:
+        logging.error('Error writing to file {}. History / Cache file not created or appended to.'.format(filename))
+
+
+def query_receipts(receipts, cached_cases):
+    updates = []
+
+    # Cycle through cases.
+    for r in receipts:
+        if not check_receipt_number(r):
+            logging.warning('Receipt number {} is not in the correct format (3 letters + 10 digits), skipping...'.format(r))
+            continue
+
+        status, info = check_status(r)
+        status_change = status != cached_cases.get(r)
+
+        timestamp = datetime.now(timezone.utc).astimezone()\
+            .replace(microsecond=0)\
+            .isoformat()
+
+        print('{}: {}, {}'.format(r, status, info))
+
+        updates.append([timestamp, r, status, info, status_change])
+
+    return updates
 
 
 if __name__ == '__main__':
-    case_id = os.getenv('USCIS_CASE_ID')
-    telegram_bot_api = os.getenv('TELEGRAM_BOT_API')
-    telegram_id = os.getenv('TELEGRAM_ID')
-    if not case_id:
-        logging.critical("No environment variable USCIS_CASE found!")
-        sys.exit(1)
-    if not telegram_bot_api:
-        logging.critical("No environment variable TELEGRAM_API found!")
-        sys.exit(1)
-    if not telegram_id:
-        logging.critical("No environment variable TELEGRAM_ID found!")
-        sys.exit(1)
-    logging.info('Monitoring USCIS case: {}'.format(case_id))
-    logging.info('BOT_API: {}, CHAT: {}'.format(telegram_bot_api, telegram_id))
-    check_periodically(case_id, telegram_bot_api, telegram_id)
+    args = parse_arguments()
+
+    # read
+    if args.file is not None:
+        cached_cases = read_cases_cache_file(args.file)
+
+    # query
+    updates = query_receipts(args.receipts, cached_cases)
+
+    # notify (on changed cases)
+    if args.telegram is not None:
+        send_notifications(args.telegram[0], args.telegram[1], [i[1:4] for i in updates if i[4] is True])
+
+    # write
+    if args.file is not None:
+        write_cases_cache_file([i[0:4] for i in updates], args.file)
